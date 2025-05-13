81fca7b461b88f4147fd91ba2c07a0ec30125d72
unblockzone <bjhuckle@students.wcpss.net>
2025-05-13 19:14:44 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+const clients = new Set();
+
+wss.on('connection', (ws) => {
+    clients.add(ws);
+    
+    ws.on('message', (message) => {
+        // Broadcast to all connected clients
+        clients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        clients.delete(ws);
+    });
+});
